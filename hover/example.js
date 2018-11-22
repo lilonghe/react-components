2b314109307fea5c2633a3582e20610590b5acca
longhe.li <lilonghe@outlook.com>
2018-11-22 16:53:31 +0800

    hover example

---
+++ hover/example.js
@@ -0,0 +1,11 @@
+<Hover panel={<div style={{display: 'flex', textAlign: 'left', alignItems: 'center'}}>
+  <img style={{marginRight: 10}} width={80} height={80} src=""/>
+  <div>
+      <p>李龙贺</p>
+      <p>上海 · xxx部</p>
+    </div>
+  </div>}>
+  <div>
+    so cool ~
+  </div>
+</Hover>
